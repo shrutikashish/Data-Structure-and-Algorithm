271be59f457a15bc6b9471e18775fa4139e0915c
shrutikashish <shruti.kashish8@gmail.com>
2025-07-11 01:14:50 +0530

    done some changes

--- Array/delete/app.js
+++ Array/delete/app.js
@@ -1,9 +1,21 @@
 let arr = [56,23,45,78,95,43,35]
 let position = 3
-for(let i= position;i<arr.length;i++){
+for(let i= position;i<arr.length-1;i++){
     
-        arr[i]=arr[i+1]
+      //  arr[i]=arr[i+1]
         
 }
 arr.length = arr.length-1
 console.log(arr)
+
+function removeEl(){
+        let data = [24,36,45,78,65,43,24]
+        let  position1 = document.getElementById("position").value;
+         position1 = parseInt(position1)
+        for(let i= position1;i<data.length-1;i++){
+                data[i]=data[i+1]
+                
+        }
+        data.length = data.length-1
+        console.log(data)
+}
